6e67ee8953e8ff5fbd21064769e7f4e5b08cbe52
Engel Nieves <124783345+Engel000@users.noreply.github.com>
2025-04-11 14:00:37 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ const navLinks = document.querySelector(".nav-links");
 menuIcon.onclick = () => {
     navLinks.classList.toggle("active");
 }
+
+//MODO OSCURO
+
+const darkModeToggle = document.querySelector("#dark-mode-toggle");
+
+darkModeToggle.onclick = () => {
+    document.body.classList.toggle("dark-mode");
+
+    // Cambia el texto del botón según el modo
+    if (document.body.classList.contains("dark-mode")) {
+        darkModeToggle.textContent = "☀️";
+    } else {
+        darkModeToggle.textContent = "🌙";
+    }
+};
